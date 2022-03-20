174271cfd0591c65520cf470af485d991623144f
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2022-03-20 18:02:25 -0300

    Criado projeto estoque

---
+++ estoquepreco/src/test/java/dev/jefferson/estoquepreco/EstoqueprecoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jefferson.estoquepreco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueprecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
